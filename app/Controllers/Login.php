26f8cb4c5ca6b621559914e3a0123c2b95443d7b
afiffarishudaifi <afiffaris5@gmail.com>
2022-05-25 22:21:54 +0700

    update

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -12,7 +12,7 @@ class Login extends BaseController
     private $loginModel=NULL;
 	private $googleClient=NULL;
 	function __construct(){
-		require_once APPPATH. "libraries/vendor/autoload.php";
+		require_once APPPATH. "Libraries/vendor/autoload.php";
 		$this->loginModel = new Model_login();
 		$this->googleClient = new \Google_Client();
 		$this->googleClient->setClientId("913013352146-6d8ktufmiaobgjj56pcrqrn9m9v871lc.apps.googleusercontent.com");
